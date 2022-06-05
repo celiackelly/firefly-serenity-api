61e3f5cfe542eac83473c9b22a835ec9d803d6e6
Celia Kelly <celiackelly@gmail.com>
2022-06-05 18:45:47 -0400

    Fixed cors error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 8000
 
 const cors = require('cors')
-app.use(cors)
+app.use(cors())
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
